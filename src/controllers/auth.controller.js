d34e70a744a20c50cc77efa78eb75a9deff0bb44
gitesh216 <109015479+gitesh216@users.noreply.github.com>
2025-07-11 12:03:54 +0530

    feat: auth controllers and routes boilerplate

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,25 @@
+import asyncHandler from "../utils/async-handler.js";
+import { ApiError } from "../utils/api-error.js";
+import { ApiResponse } from "../utils/api-response.js";
+
+
+const registerUser = asyncHandler(async (req, res) => {
+
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+
+});
+
+const getApiKey = asyncHandler(async (req, res) => {
+
+});
+
+const getProfile = asyncHandler(async (req, res) => {
+
+});
+
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,4 +2,10 @@ import { Router } from "express";
 
 const router = Router();
 
+router.post("/register", );
+router.post("/login", );
+router.post("/logout", );
+router.post("api-key", );
+router.get("/profile", );
+
 export default router;
